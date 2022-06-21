671a63a554710bcfcf7aa590b4662622f52271fe
Carlo Aspren Gines Lara <cagl_88@outlook.com>
2022-06-21 18:50:56 +0200

    Now scritp uses again pulseaudio tools (many things broken without)

--- changevolume.sh
+++ changevolume.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-# add this to work directly on alsa card (not with pipewire)
-DEFCARD="-c 1"
+PERCENTAGE=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//')
 
 case $1 in
 	high)
 		# Set the volume on (if it was muted)
-		amixer -q $DEFCARD sset Master unmute
-		amixer -q $DEFCARD sset Master 5%+
+		pactl set-sink-mute @DEFAULT_SINK@ 0
+	    [ $PERCENTAGE -lt 100 ] && pactl set-sink-volume @DEFAULT_SINK@ +5%
 		;;
 	low)
 		# Set the volume on (if it was muted)
-		amixer -q $DEFCARD sset Master unmute
-		amixer -q $DEFCARD sset Master 5%-
+		pactl set-sink-mute @DEFAULT_SINK@ 0
+	    pactl set-sink-volume @DEFAULT_SINK@ -5%
 		;;
 	muted)
-		amixer -q $DEFCARD sset Master toggle
+		pactl set-sink-mute @DEFAULT_SINK@ toggle
 		;;
 esac
 
-PERCENTAGE=$(amixer $DEFCARD get Master | grep Mono: | awk '{print $4}' | cut -c 2- | head -c -3)
-STATUS=$(amixer get $DEFCARD Master | grep Mono: | awk '{print $6}')
+PERCENTAGE=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//')
+STATUS=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
 
 VOLUME="muted"
 [ $PERCENTAGE -gt 0  ] && VOLUME="low"
 [ $PERCENTAGE -gt 24 ] && VOLUME="medium"
 [ $PERCENTAGE -gt 49 ] && VOLUME="high"
 [ $PERCENTAGE -gt 74 ] && VOLUME="overamplified"
-[ $STATUS = "[off]" ] && VOLUME="muted"
+[ $STATUS = "yes" ] && VOLUME="muted"
 
 dunstify -a "multimedial" -r "9993" -h int:value:"$PERCENTAGE" -i "~/Pictures/popcatpng/$VOLUME.png" "Volume $VOLUME: $PERCENTAGE%"
