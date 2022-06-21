77eebde604b0cbc75b83d739ad962bae105bac89
Carlo Aspren Gines Lara <cagl_88@outlook.com>
2022-06-21 11:53:27 +0200

    Removed useless stuff

--- startway.sh
+++ startway.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-#waybar &
+waybar &
 wbg ~/Pictures/spiritedtrain.jpg &
 wlsunset -L 41.50 -l 12.30 &
 dunst &
-#foot --server &
 gentoo-pipewire-launcher &
-
-#test ! $(pgrep swhks) && swhks &
-#pkexec swhkd
